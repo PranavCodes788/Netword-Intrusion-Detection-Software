f87272f2aa910e0e4497b9b11004f4b9c89a3fa8
Pranav Palle <pranav.palle10@gmail.com>
2024-12-30 23:46:26 -0500

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,9 +1,17 @@
 # capture.py
+
+#Handles live packet capturing using Scapy.
+    
 from scapy.all import sniff
 from main import process_packet
 
+# Captures live network packets and processes them with a callback function.
+
 def live_capture():
     print("Starting live capture...")
+
+     # Sniff packets and call the callback for each one
+    
     sniff(filter="ip", prn=handle_packet, store=0)
 
 def handle_packet(pkt):
