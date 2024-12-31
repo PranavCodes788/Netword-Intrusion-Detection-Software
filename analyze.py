5422e29916214a0581a7c7b0ee6d8f2e93ff8541
Pranav Palle <pranav.palle10@gmail.com>
2024-12-30 23:47:30 -0500

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,16 +1,12 @@
-
+#Performs threat analysis on network packets.
 
 known_patterns = [
     "malicious_payload_1",
     "malicious_payload_2"
 ]
-
+# Analyzes a packet and determines its safety.
 def analyze_data(fake_ip, payload):
-    """
-    We pretend to analyze 'fake_ip' and 'payload'.
-    Returns "Intrusion Detected" if any known pattern is found.
-    Otherwise returns "Safe".
-    """
+
     for pattern in known_patterns:
         # If malicious pattern is in the payload, flag as Intrusion
         if pattern in payload:
