a7daf1d02d356785c5c2f6989b8192dbeb4bdba8
Pranav Palle <pranav.palle10@gmail.com>
2024-12-30 23:50:45 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
 # dashboard.py
-
+#Flask-based web dashboard to display packet logs from the SQLite database.
 from flask import Flask, render_template
 import sqlite3
 
@@ -7,18 +7,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    """
-    Connect to the database and retrieve all logs,
-    then pass them to the 'dashboard.html' template.
-    """
+     #Renders the dashboard HTML template with packet logs from the database.
     conn = sqlite3.connect("../nids_logs.db")
+
+      # Fetch all logs from the database
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM logs")
     logs = cursor.fetchall()  # Each row is (id, fake_ip, payload, status)
     conn.close()
-
+ # Pass logs to the template
     return render_template("dashboard.html", logs=logs)
 
 if __name__ == "__main__":
     print("Starting Flask Dashboard at http://127.0.0.1:5000")
+     # Run the Flask server
     app.run(debug=True)
